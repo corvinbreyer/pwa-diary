00e5bb7b57326cb3644c8bdbddc6cd195feba3f3
Corvin Breyer <josubowsu@gmail.com>
2025-03-21 17:14:55 +0100

    extract css variables and add basic chart

---
+++ _moodtracker/mainChart.js
@@ -0,0 +1,29 @@
+function cssVar(s) {
+    return window.getComputedStyle(document.body).getPropertyValue(s);
+}
+const ctx = document.getElementById('myChart');
+const myChart = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'],
+    values: [12, 19, 3, 5, 2, 3],
+};
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: myChart.labels,
+        datasets: [{
+            label: '# of Votes',
+            data: myChart.values,
+            backgroundColor: cssVar('--primary-500') + '33',
+            borderColor: cssVar('--primary-500'),
+            borderWidth: 1
+                }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
