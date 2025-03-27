7b26c86f024a4b81ee59dfa53f5cc9333b34a036
Corvin Breyer <josubowsu@gmail.com>
2025-03-27 19:59:21 +0100

    Revert "make file references more stable"
    This reverts commit 5a8bd30ad00e66457c5aa18db3587e5ea1bbe0bf.

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,21 @@ const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
     
-    '/pwa-diary/css/variables.css',
-    '/pwa-diary/css/custom.css',
-    '/pwa-diary/css/theme.css',
+    '/css/variables.css',
+    '/css/custom.css',
+    '/css/theme.css',
     
-    '/pwa-diary/js/calendar.js',
-    '/pwa-diary/js/customization.js',
-    '/pwa-diary/js/database.js',
-    '/pwa-diary/js/installButton.js',
-    '/pwa-diary/js/mainChart.js',
-    '/pwa-diary/js/navigation.js',
+    '/js/calendar.js',
+    '/js/customization.js',
+    '/js/database.js',
+    '/js/installButton.js',
+    '/js/mainChart.js',
+    '/js/navigation.js',
     
-    '/pwa-diary/icons/icon-192x192.png',
-    '/pwa-diary/icons/icon-512x512.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
     
-    '/pwa-diary/manifest.json'
+    '/manifest.json'
 ];
 
 // Install event - cache app shell
