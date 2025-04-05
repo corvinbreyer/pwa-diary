643fc4da58bca0053ddebf63ab1884df3ea942da
Corvin Breyer <josubowsu@gmail.com>
2025-04-05 15:36:14 +0200

    update chart styling

--- js/mainChart.js
+++ js/mainChart.js
@@ -1,29 +1,54 @@
-function cssVar(s) {
-    return window.getComputedStyle(document.body).getPropertyValue(s);
-}
 const ctx = document.getElementById('myChart');
-const myChart = {
-    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'],
-    values: [12, 19, 3, 5, 2, 3],
-};
-
+const values = [12, 19, 3, 5, 2, 3];
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: myChart.labels,
+        labels: ['😡', '😟', '😐', '🙂', '😄'],
         datasets: [{
             label: '# of Votes',
-            data: myChart.values,
-            backgroundColor: cssVar('--primary-2'),
-            borderColor: cssVar('--primary-2'),
+            data: values,
+            backgroundColor: '#4dd0e1',
+            borderColor: '#0097a7',
             borderWidth: 1
                 }]
     },
     options: {
+        plugins: {
+            legend: {
+                display: false //hides the filtering option
+            }
+        },
         scales: {
+            x: {
+                ticks: {
+                    font: {
+                        size: 20 //sets the label font size
+                    }
+                }
+            },
             y: {
                 beginAtZero: true
             }
         }
-    }
+    },
+    plugins: [{
+        afterDraw: function (chart) {
+            const ctx = chart.ctx;
+            ctx.font = '14px Arial';
+            ctx.textAlign = 'center';
+
+            chart.data.datasets[0].data.forEach((value, index) => {
+                const x = chart.scales.x.getPixelForValue(index);
+                const y = chart.scales.y.bottom - 25; // Position above the X-axis labels
+                const width = 25;
+                const height = 20;
+
+                ctx.fillStyle = '#b2ebf2';
+                ctx.fillRect(x - width / 2, y, width, height);
+
+                ctx.fillStyle = '#006064';
+                ctx.fillText(value, x, y + height / 1.5);
+            });
+        }
+  }]
 });
